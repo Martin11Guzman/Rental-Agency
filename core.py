62dd283ec5507f8ab4134101b0d7ae04b9d4d052
Unknown <mguzman@basecampcodingacademy.org>
2017-02-16 14:21:36 -0600

    made  a renovate function that takes in class attributes to update.

--- core.py
+++ core.py
@@ -1,50 +1,89 @@
-# from Classes import *
-# from inventory import *
-# import sys
-# import csv
-# import os
-# import datetime
-# import time
-#
-#
-#
-# def menu():
-#     print("Hello how you doing!??")
-#     time.sleep(1)
-#     print("Welcome to Guz's Medical Equipment Rental Agency. ")
-#     time.sleep(1)
-#     print("How can I help you today?")
-#     choice = input("are you renting or returning an item?\n").strip().lower()
-#     if choice == "rent":
-#         check_if_files_exist()
-#         rent()
-#     elif choice == "return":
-#         return_item()
-#     elif choice == "q":
-#         print("System closing...")
-#         sys.exit()
-#     else:
-#         print("\nINVALID INPUT\n")
-#         print('did you mean rent or return? ')
-#
-#
-# menu()
-#
-#
-# def get_file_contents():
-#
-#
-#
-#
-# def get_item_by_name
-#
-#
-# def update inventory
-#
-# def transaction
-# def update deposits
-# restart()
-#
-#
-#
-#
+from Classes import *
+from inventory import *
+import sys
+import csv
+import os
+from inventory import *
+from Classes import *
+import datetime
+import time
+
+
+
+def menu():
+    print("Hello how you doing!??")
+    time.sleep(1)
+    print("Welcome to Guz's Medical Equipment Rental Agency. ")
+    time.sleep(1)
+    print("How can I help you today?")
+    choice = input("are you renting or returning an item?\n").strip().lower()
+    if choice == "rent":
+        check_if_files_exist()
+        rent()
+    elif choice == "return":
+        return_item()
+    elif choice == "q":
+        print("System closing...")
+        sys.exit()
+    else:
+        print("\nINVALID INPUT\n")
+        print('did you mean rent or return? ')
+
+
+menu()
+
+
+def rent():
+    "Input determines what item is being rented"
+    show = data_from_file('inventory.csv')
+    print(view_inv(show))
+    item = input("What will you be renting? Product name: ").strip().capitalize()
+    if item == "Q":
+        print('System closing...')
+        sys.exit()
+    else:
+        customer_choice = choose_item(data_from_file('inventory.csv'), item)
+        if customer_choice == None:
+            print("\nInvalid choice " + item + " is not in inventory\n")
+            rent()
+        else:
+            print(customer_choice.deposit_value, customer_choice.price)
+            print("You must place a deposit of $", customer_choice.deposit_value,\
+                "along with a fee of $", customer_choice.price,\
+                "every hour the item is rented. Deposits are refunded upon return.\n")
+            print("Confirm you purchase for\n", str(customer_choice))
+            confirmation = input('y/n\n').strip().lower()
+            if confirmation == "y":
+                renovate_inventory(customer_choice.name, \
+                int(customer_choice.quantity)- 1, 'inventory.csv')
+                renovate_transaction(datetime.datetime.now(),\
+                 customer_choice.name, "pending", 'transaction.csv')
+                update_deposits(customer_choice.deposit_value, 'deposit.csv')
+                restart()
+            elif confirmation == "n":
+                restart()
+            elif confirmation == "q":
+                print("System closing...")
+                sys.exit()
+            else:
+                print('invalid entry')
+                restart()
+
+
+def restart():
+    print("\nType: rent to rent an item\nType: return to return an item\nType: start to restart program\nType: q to quit\n")
+    choice = input().strip().lower()
+    if choice == "rent":
+        rent()
+    elif choice == "return":
+        return_item()
+    elif choice == "start":
+        start()
+    elif choice == "q":
+        print('System closing....')
+    else:
+        print("invalid input")
+        restart()
+
+
+
--- inventory.py
+++ inventory.py
@@ -60,49 +60,33 @@ def choose_item(inventory_list, name):
             customer_item = None
         return customer_item
 
+def update_deposits(deposit, f):
+    return write_row(f, [[deposit]])
+
+def update_revenue(rent, sales_tax, f):
+    return write_row(f, [[rent, sales_tax]])
+
+def renovate_inventory(name, quantity, f):
+    stock = []
+    inv = data_from_file(f)
+    for i in inv:
+        stock.append(Rental(i[0], i[1], i[2], i[3], i[4]))
+    for i in stock:
+        if i.name == name:
+            i.quantity = quantity
+    file = open(f, 'w')
+    writer = csv.writer(file, delimiter=',')
+    for i in stock:
+        writer.writerow([i.name, i.replacement_value, i.deposit, i.price, i.quantity])
+    file.close()
+    with open(f) as file:
+        inv = file.read()
+        return inv
+
+def renovate_transaction(f, date, item, status ):
+    return write_row(f, [[date, item, status]])
 
 
 
 
 
-
-
-
-
-
-
-
-
-def rent():
-    show = data_from_file('inventory.csv')
-    print(view_inv(show))
-    item = input("What will you be renting? Product name: ").strip().capitalize()
-    if item == "Q":
-        print('System closing...')
-        sys.exit()
-    else:
-        customer_choice = get_item_by_name(data_from_file('inventory.csv'), item)
-        if customer_choice == None:
-            print("\nInvalid choice " + item + " is not in inventory\n")
-            rent()
-        else:
-            print(customer_choice.deposit_value, customer_choice.price)
-            print("You must place a deposit of $", customer_choice.deposit_value, "along with a fee of $",
-                  customer_choice.price, "every hour the item is rented. Deposits are refunded upon return.\n")
-            print("Confirm you purchase for\n", str(customer_choice))
-            confirmation = input('y/n\n').strip().lower()
-            if confirmation == "y":
-                update_inventory(customer_choice.name, int(customer_choice.quantity) - 1, 'inventory.csv')
-                update_transaction(datetime.datetime.now(), customer_choice.name, "pending", 'transaction.csv')
-                update_deposits(customer_choice.deposit_value, 'deposit.csv')
-                restart()
-            elif confirmation == "n":
-                restart()
-            elif confirmation == "q":
-                print("System closing...")
-                sys.exit()
-            else:
-                print('invalid entry')
-
-
-restart()

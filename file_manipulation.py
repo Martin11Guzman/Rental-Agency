80564c70e0eef2d41b6faa8d556cb82e64878689
Unknown <mguzman@basecampcodingacademy.org>
2017-03-01 09:11:57 -0600

    took out choose_item func and added show_transaction also added comments

--- file_manipulation.py
+++ file_manipulation.py
@@ -3,11 +3,17 @@ import datetime
 import csv
 import time
 
+"""This file involves functions that directly communicate to the percistent files that collect data"""
+
+
 def check_if_files_exist():
+    """ None ->  None
+    function checks path to see if files exist
+
+                    """
     datafile="/home/basecamp/Desktop/Rental-Agency/inventory.csv"
     try:
         make_data_files(datafile)
-        make_data_files('revenue.csv')
         make_data_files('deposit.csv')
         make_data_files('transaction.csv')
         make_rows("inventory.csv", inv_items())
@@ -18,10 +24,12 @@ def check_if_files_exist():
        print("Error: %s not found." % datafile)
 
 def make_data_files(f):
+    """(f) -> None"""
     with open(f, 'w') as files:
         return files.close()
 
 def data_from_file(f):
+    """ (list of lists) -> (Rental)"""
     with open(f, newline='') as data:
         invent = csv.reader(data, delimiter=',', quotechar='|')
         return list(invent)
@@ -29,6 +37,7 @@ def data_from_file(f):
     #     invent = pickle.load(f)
 
 def make_rows(f, attributes):
+    """(f, attributes) -> None"""
     with open(f, 'a', newline='') as file:
         writer = csv.writer(file)
         for row in attributes:
@@ -38,27 +47,22 @@ def make_rows(f, attributes):
         return data
 
 def update_deposits(deposit, f):
+    """(deposit(int), file) -> None"""
     return make_rows(f, [[deposit]])
 
+def show_transaction(transactions):
+    """datetime, Rental(obj), status(str) -> None"""
+    trans_string = ""
+    for transaction in transactions:
+        trans_string += ("\nDatetime: " + str(transaction[0]) + "\nRental:" + str(transaction[1]) +
+                                         "\nstatus " + str(transaction[2]) + "\n")
+    return trans_string
 
-
-def choose_item(inventory_list, name):
-    customer_item = ''
-    for i in inventory_list:
-        customer_item = Rental(i[0], i[1], i[2], i[3], i[4])
-        if customer_item.name == name:
-            return customer_item
-        else:
-            customer_item = None
-    return customer_item
 
 
 def renovate_transaction(date, item, status, f):
+    """datetime, Rental(obj), status(str) -> list"""
     return make_rows(f, [[date, item, status]])
 
-
-
-def update_revenue(rent, sales_tax, f):
-    return make_rows(f, [[rent, sales_tax]])
 
 
